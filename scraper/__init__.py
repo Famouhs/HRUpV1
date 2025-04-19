efa831b1953e5020e04faf27d62726c27b2438d3
Famouhs <Alvarez.Jeanp@gmail.com>
2025-04-19 15:17:30 -0400

    Update __init__.py

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,7 +2,8 @@
 
 # This file makes the `scraper` directory a Python package
 
-from .odds_scraper import get_odds_data
+from scraper.odds_scraper import get_odds_data
+
 from .weather_scraper import get_weather_data
 
 __all__ = ["get_odds_data", "get_weather_data"]
